f9acf3a9571c171762d403ff00abee344f6763a0
genesissiana <perezgenesisa@gmail.com>
2021-09-16 10:08:25 -0700

    render gallery pages

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { capitalizeFirstLetter } from '../../utils/helpers';
+import photo from "../../assets/small/commercial/0.jpg";
+
 
 function Gallery(props) {
   const currentCategory = {
@@ -8,8 +11,15 @@ function Gallery(props) {
   };
   return (
     <section>
-      <h1>{currentCategory.name}</h1>
+      <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
       <p>{currentCategory.name}</p>
+      <div className="flex-row">
+          <img
+            src={photo}
+            alt="Commercial Example"
+            className="img-thumbnail mx-1"
+          />
+      </div>
     </section>
   );
 }
